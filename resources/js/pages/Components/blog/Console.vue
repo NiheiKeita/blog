07a0bb9b153f740c33297a3c8fdcf497a7ca63e0
NiheiKeita <86122925+NiheiKeita@users.noreply.github.com>
2024-01-20 00:34:18 +0900

    コンソールに謎の空欄が入っているバグ修正と、コンソール内の行間を調整

--- resources/js/pages/Components/blog/Console.vue
+++ resources/js/pages/Components/blog/Console.vue
@@ -8,7 +8,9 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="bg-black text-white rounded-md p-3 mb-5 overflow-x-auto">
-        <code class="p-1 mx-1 whitespace-pre"> <slot /></code>
+    <div
+        class="bg-black text-white rounded-md p-3 mb-5 overflow-x-auto lg:leading-7 leading-6"
+    >
+        <code class="whitespace-pre"><slot /></code>
     </div>
 </template>
