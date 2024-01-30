530b527e67279b58e70491282b51867dfb4dd321
NiheiKeita <86122925+NiheiKeita@users.noreply.github.com>
2024-01-31 00:37:44 +0900

    デザインとかを少し修正

--- resources/js/pages/Admin/Comment/Show.vue
+++ resources/js/pages/Admin/Comment/Show.vue
@@ -6,30 +6,30 @@ const props = defineProps({ comment: Object });
 
 <template>
     <Layout>
-        <div class="mb-20">
+        <div class="mb-20 bg-white">
             <div class="flex justify-center mt-20">
                 <div class="w-full lg:w-3/4 min-h-full p-8">
-                    <div>コメント</div>
+                    <div>コメント内容</div>
                     <div class="mt-5 ml-5">{{ comment.comment }}</div>
-                    <div class="mt-5">email</div>
-                    <div class="mt-5 ml-5">
-                        {{ comment.email ?? "--空欄--" }}
-                    </div>
-                    <div class="mt-5">name</div>
-                    <div class="mt-5 ml-5">
-                        {{ comment.name ?? "--空欄--" }}
-                    </div>
                     <div class="mt-5">ブログID</div>
                     <div class="mt-5 ml-5">
                         {{ comment.id }}
                     </div>
-                    <div class="mt-5">
+                    <div class="mt-5 ml-5">
                         <a
                             :href="route('blog.show', comment.id)"
                             target="_blank"
                             class="text-blue-500 hover:underline"
                             >{{ route("blog.show", comment.id) }}</a
                         >
+                    </div>
+                    <div class="mt-5">名前</div>
+                    <div class="mt-5 ml-5">
+                        {{ comment.name ?? "--空欄--" }}
+                    </div>
+                    <div class="mt-5">返信用email</div>
+                    <div class="mt-5 ml-5">
+                        {{ comment.email ?? "--空欄--" }}
                     </div>
                 </div>
             </div>
